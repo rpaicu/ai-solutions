cf35802d161a19f860931708211f41ee3b4330d8
Pavel Zloi <github.com@drteam.rocks>
2025-04-13 15:45:39 +0300

    Create openai-client.py

---
+++ openai-client.py
@@ -0,0 +1,62 @@
+import os
+import argparse
+import time
+
+from openai import OpenAI
+
+OPENAI_BASE_URL = os.environ.get('OPENAI_BASE_URL', 'https://api.rpa.icu')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', 'https://t.me/evilfreelancer')
+DEFAULT_MODEL = 'large-v3-turbo'
+DEFAULT_LANGUAGE = "ru"
+
+
+def main():
+    start_time = time.time()
+
+    # Read the file from the file path by arguments of argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str)
+    parser.add_argument('--format', type=str, choices=['text', 'json', 'srt', 'verbose_json'], default='text')
+    parser.add_argument('--model', type=str, default=DEFAULT_MODEL)
+    parser.add_argument('--language', type=str, default=DEFAULT_LANGUAGE)
+    args = parser.parse_args()
+
+    # Read the file from the file path
+    input_file = open(args.file, "rb")
+
+    # Initialize client
+    client = OpenAI(base_url=OPENAI_BASE_URL, api_key=OPENAI_API_KEY)
+
+    # Run the transcription with time measurement
+    transcription = client.audio.transcriptions.create(
+        language=args.language,
+        file=input_file,
+        model=args.model,
+        response_format=args.format,
+    )
+
+    # Save the transcription to the file
+    if args.format == 'text':
+        extension = 'txt'
+    elif args.format == 'srt':
+        extension = 'srt'
+    elif args.format == 'vtt':
+        extension = 'vtt'
+    elif args.format == 'json' or args.format == 'verbose_json':
+        extension = 'json'
+    else:
+        raise ValueError('Invalid format')
+
+    # Remove extension from file name
+    output_file = f'{os.path.splitext(args.file)[0]}.{extension}'
+    with open(output_file, 'w') as f:
+        f.write(transcription)
+        print(f'Saved transcription to {output_file}')
+
+    # Print time elapsed for transcription
+    total_elapsed = time.time() - start_time
+    print(f"\nTotal execution time: {total_elapsed:.2f} seconds")
+
+
+if __name__ == '__main__':
+    main()
